5afbbc26bd3df9e031ec5af3f5016a8dd5741e1e
Dung Nguyen Quoc <dungnq@tech.est-rouge.com>
2023-03-22 21:53:25 +0700

    Update fastapi docker

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, status, Request, HTTPException, Depends, UploadFile, File
 import json
 from typing import List
-from utils.constants import AUTH_USER, AUTH_PWD, save_path
+from util.constants import AUTH_USER, AUTH_PWD, save_path
 import datetime
 from fastapi.openapi.utils import get_openapi
 from starlette.responses import RedirectResponse, Response, JSONResponse
@@ -23,12 +23,7 @@ from insightface_func.face_detect_crop_single import Face_detect_crop
 from util.videoswap import video_swap
 import os
 
-opt = TestOptions().parse()
-opt = TestOptions()
 
-opt.initialize()
-opt.parser.add_argument('-f') ## dummy arg to avoid bug
-opt = opt.parse()
 
 app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
 
@@ -43,10 +38,6 @@ transformer_Arcface = transforms.Compose([
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
 
-# class video_store(BaseModel):
-#     video_id: str
-#     video_path: str
-#     image: File 
 
 def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
     correct_username = secrets.compare_digest(credentials.username, AUTH_USER)
@@ -103,13 +94,26 @@ def store_video(video: video_store,auth=Depends(get_current_username)):
 
 
 @app.post("/video/swap")
-def submit(video: video_store = Depends(), files: List[UploadFile] = File(...)):
+async def submit(video: video_store = Depends(), file: UploadFile = File(...)):
     received_data= video.dict()
-    video_path = save_path + received_data["video_id"]+".mp4"
-    opt.pic_a_path = files[0] ## or replace it with image from your own google drive
+    print(received_data)
+    opt = TestOptions()
+    opt.initialize()
+    opt = opt.parse()
+    isExist = os.path.exists(save_path)
+    if not isExist:
+    # Create a new directory because it does not exist
+        os.makedirs(save_path)
+    #todo: check whether the video exist or not to re-download
+
+    video_path = save_path + "/"+ received_data["video_id"]+".mp4"
+
     opt.video_path = video_path ## or replace it with video from your own google drive
     opt.output_path = './output/demo2.mp4'
-    crop_size = opt.crop_size
+    crop_size = 224
+
+    contents = await file.read()
+    nparr = np.frombuffer(contents, np.uint8)
 
     torch.nn.Module.dump_patches = True
     if crop_size == 512:
@@ -125,8 +129,7 @@ def submit(video: video_store = Depends(), files: List[UploadFile] = File(...)):
     app = Face_detect_crop(name='buffalo_sc', root='./insightface_func/models')
     app.prepare(ctx_id= 0, det_thresh=0.6, det_size=(640,640),mode=mode)
     with torch.no_grad():
-        pic_a = opt.pic_a_path
-        img_a_whole = cv2.imread(pic_a)
+        img_a_whole = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
         img_a_align_crop, _ = app.get(img_a_whole,crop_size)
         img_a_align_crop_pil = Image.fromarray(cv2.cvtColor(img_a_align_crop[0],cv2.COLOR_BGR2RGB)) 
         img_a = transformer_Arcface(img_a_align_crop_pil)
@@ -142,5 +145,5 @@ def submit(video: video_store = Depends(), files: List[UploadFile] = File(...)):
 
         video_swap(opt.video_path, latend_id, model, app, opt.output_path,temp_results_dir=opt.temp_path,\
             use_mask=opt.use_mask,crop_size=crop_size)
-    return {"JSON Payload ": received_data, "Uploaded Filenames": [file.filename for file in files]}
+    return {"status":200}
 
